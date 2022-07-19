0ce573587f74c9ea42fdb675890492b62881ec98
guilherme <guigarks@gmail.com>
2022-07-19 16:29:20 -0300

    Date select

--- backend/src/main/java/com/devsuperior/dsmeta/service/SaleServices.java
+++ backend/src/main/java/com/devsuperior/dsmeta/service/SaleServices.java
@@ -1,8 +1,14 @@
 package com.devsuperior.dsmeta.service;
 
-import java.util.List;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -13,9 +19,15 @@ import com.devsuperior.dsmeta.repositories.SaleRepository;
 public class SaleServices {
 	
 	@Autowired
-	private SaleRepository repository;
+	private SaleRepository repository;		
 	
-	public List <Sale> findSales(){
-		 return repository.findAll();
+	public Page<Sale>findSales(String minDate, String maxDate,  Pageable pageable){
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault()); // Tratar caso de data em branco na requisição.
+		
+		LocalDate min = minDate .equals("") ? today.minusDays(365): LocalDate.parse(minDate);
+		LocalDate max = maxDate .equals("") ? today: LocalDate.parse(maxDate);//test se data em branco aplicar a data today.
+		
+		 return repository.findSales(min, max, pageable);
 	 	}
 }
